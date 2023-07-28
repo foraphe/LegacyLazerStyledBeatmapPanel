90ef4b28965daf2ed933f64965c4363d54c2db4c
foraphe <me@foraphe.eu.org>
2023-07-28 16:00:04 +0800

    [fix] increase .osu file process delay so that gosumemory have enough time to update its own data source

--- js/class.js
+++ js/class.js
@@ -83,7 +83,7 @@ function Ticker(interval) {
                                     }
                                 });
                         });
-                }, 100);
+                }, 500);
             }
         }
     }
